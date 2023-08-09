bd069cb304e1b8523b627efb5f738a1a7bff25c9
Arjun Krishnakumar <arjunkrishnakumar96.mec@gmail.com>
2023-08-09 18:04:30 -0500

    sample grpc

---
+++ GrpcService/ClientApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using Grpc.Net.Client;
+using GrpcService;
+using System;
+using System.Threading;
+
+namespace ClientApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+
+            var reply = client.SayHello(
+                new HelloRequest { Name = "Test" });
+
+            Console.WriteLine("From Server: " + reply.Message);
+        }
+    }
+}
